402349a6dee64385c3301ca408fad2b06208c1bf
petrcada <error414@error414.com>
2015-08-23 11:54:21 +0200

    updatovana verze pro 1.3

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,16 @@
 $(function(){
 
+		$('.res  tr,h3,table').filter(function(){
+			return (
+				$(this).attr('data-path').contains('subtrim') ||
+				$(this).attr('data-path').contains('cyclic_ring_range') ||
+				$(this).attr('data-path').contains('servo_travel_correction') ||
+				$(this).attr('data-path').contains('rudder_end_points_no_break') ||
+				$(this).attr('data-path').contains('channels') ||
+				$(this).attr('data-path').contains('correction') ||
+				$(this).attr('data-path').contains('limits')
+				 );
+		}
+		).hide();
+
 });
